38e66b8c56b04b44d89c79069df6d6bec50725f9
Vladislau Zhuk <86854997+SnikersFar@users.noreply.github.com>
2022-03-18 10:51:14 +0300

    add changint height chat

--- OnlineChat/wwwroot/js/site.js
+++ OnlineChat/wwwroot/js/site.js
@@ -32,7 +32,7 @@
             list.append(elem);
 
             const topPosition = $(".message:last").position().top;
-            $(".chat_list").animate({ scrollTop: topPosition }, 300);
+            $(".chat_list").animate({ scrollTop: $(document).height() * $(document).height() * $(document).height() }, 300);
         }
 
     });
@@ -68,7 +68,110 @@
     }
 
 
+    // scrolling change height chat
+    //var p = $(".pan1");
+    //var d = $(".pan2");
+    //var r = $("#resize");
+    //var curr_height = p.height()
+    //var unlock = false;
+    //var linkedMouse = 0;
+    //var heightMover = 1;
+    //$(document).mousemove(function (e) {
+    //    var WindowHeight = window.innerHeight * 0.8;
+    //    /*   var change = curr_height + (e.clientY - curr_height);*/
+    //    var myHeight = r.css("height").replace("px", '');
 
+    //    console.log("Screen Y: " + window.innerHeight);
+    //    console.log("Height Mover: " + heightMover);
+    //    console.log("linkedMouse: " + linkedMouse);
+    //    console.log("Height: " + myHeight);
+
+    //    if (unlock) {
+    //        if (myHeight < WindowHeight) {
+    //            heightMover = heightMover + (e.clientY - linkedMouse)*0.05;
+    //            $("#debug").text(e.clientX + " resize");
+    //            p.css("height", heightMover);
+    //            //d.css("margin-left", change);
+    //        }
+    //        else {
+
+    //        }
+
+    //    }
+    //    if (r.css("height").replace("px", '') >= WindowHeight) {
+    //        let myheight = WindowHeight - 10;
+    //        r.css("height", myheight);
+
+    //    }
+    //    if (r.css("height").replace("px", '') <= 200) {
+    //        r.css("height", 201);
+    //    }
+    //});
+    //r.mousedown(function (e) {
+    //    curr_width = p.width();
+    //    linkedMouse = e.clientY;
+    //    unlock = true;
+    //    r.css("background-color", "rgba(0, 0, 0, 0.2)");
+    //});
+    //$(document).mousedown(function (e) {
+    //    if (unlock) {
+    //        e.preventDefault();
+    //    }
+    //});
+    //$(document).mouseup(function (e) {
+    //    unlock = false;
+    //    $("#debug").text("");
+    //    r.css("background-color", "rgba(0, 0, 0, 0.1)");
+    //});
+    var p = $(".pan1");
+    var d = $(".pan2");
+    var r = $("#resize");
+
+    var cur_height = p.height()
+    var unlock = false;
+    var linked_heightMouse;
+    $(document).mousemove(function (e) {
+
+        var change = cur_height + (e.clientY - linked_heightMouse);
+
+        //console.log("p.height(): " + p.height());
+        //console.log("e.clientY: " + e.clientY);
+        //console.log("cur_height: " + cur_height);
+        //console.log("linked_heightMouse: " + linked_heightMouse);
+        //console.log("window.innerHeight: " + window.innerHeight);
+
+        if (unlock) {
+
+            $("#debug").text(e.clientX + " resize");
+            p.css("height", change);
+        }
+        if (p.height >= window.innerHeight) {
+            p.height = window.innerHeight;
+        }
+    });
+
+    $(window).resize(function () {
+        if (p.height >= window.innerHeight) {
+            p.height = window.innerHeight;
+        }
+    });
+
+    r.mousedown(function (e) {
+        linked_heightMouse = e.clientY;
+        cur_height = p.height();
+        unlock = true;
+    });
+
+    $(document).mousedown(function (e) {
+        if (unlock) {
+            e.preventDefault();
+        }
+    });
+
+    $(document).mouseup(function (e) {
+        unlock = false;
+        $("#debug").text("");
+    });
 
 
 
